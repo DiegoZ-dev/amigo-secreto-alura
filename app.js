0d5f86a503d30fe1ec1d2fc1576de40d05c2b9cc
Diego Zarate <dienzgo97@gmail.com>
2025-08-22 20:59:10 -0300

    Añadir nombre a la lista

--- app.js
+++ app.js
@@ -1,1 +1,34 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigos = [];
+let inputElement = document.getElementById("amigo");
+
+inputElement.addEventListener('input',function(){
+        this.value = this.value.replace(/[^A-Za-zÁÉÍÓÚáéíóúÑñ\s]/g,''); //Si entra un número en el input, no lo escribe.
+    });
+
+function actualizarLista() {
+    //Limpiar la lista
+    let listaElem = document.getElementById("listaAmigos");
+    listaElem.innerHTML = '';
+
+    //Obtengo cada amigo en una lista
+    let strAmigo = '';
+    for (let i = 0; i < amigos.length; i++) {
+        const amigo = amigos[i];
+        strAmigo = `${strAmigo}<li>${amigo}</li>`;
+    }
+    //Los inserto en la lista del documento
+    listaElem.innerHTML = strAmigo;
+}
+
+function agregarAmigo() {
+    let txtAmigo = inputElement.value; //Obtengo la variable ya previamente controlada para que solo tenga letras
+
+    if(txtAmigo == ""){
+        alert("Por favor, inserte un nombre.");
+    }
+    else {
+        amigos.push(txtAmigo);
+        inputElement.value = '';
+        actualizarLista();
+    }
+}
